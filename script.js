7dc4ed3db0430dd2b68d7bb63e6a0fdbd7b2954c
rahimi <rahimi.engineer.1378@gmail.com>
2023-04-26 10:57:16 +0430

    empty board has been corrected

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 let addButtom = document.querySelector('.add-note')
 let addNoteForm = document.querySelector('.overlay')
-let bord = document.querySelector('#bord')
+let board = document.querySelector('#board')
 let done = document.querySelector('#done')
 let close = document.querySelector('#close')
 let noteList = document.querySelector('.note-list')
 let  textarea = document.querySelector('textarea')
 
+let childCount = noteList.childElementCount
+let emptyError = document.createElement('div')
 
 addButtom.addEventListener('click',addNote)
 
 done.addEventListener('click',doneNote)
 close.addEventListener('click',closeNote)
 
+window.addEventListener('load',empty)
+
+function empty(){
+    if(!noteList.children.length){
+        emptyError.innerText = 'your board is Empty!!!'
+        emptyError.classList.add('emptyError')
+        noteList.appendChild(emptyError)
+    }
+    else{
+        emptyError.remove()
+    }
+}
+
 function addNote(){
     addNoteForm.classList.remove("display-none")
-    bord.classList.add('disable')  
+    board.classList.add('disable')  
 }
 
 function doneNote(){
@@ -22,24 +37,30 @@ function doneNote(){
     let newNote = document.createElement('div')
     let textNote = document.createElement('p')
     let colors = ['#c0ed38','#ff59ee','#34b6ff','#01bd1e','#bf8b1b']
-
-    if(textarea.value.length < 90  && !textarea.value == ''){
+    
+    if(textarea.value.length < 90  && !textarea.value == '' && textarea.value){
+        
+        
         newNote.classList.add('note')
         textNote.innerText = textarea.value
         textNote.style.wordBreak = 'break-all';
         
-        let childCount = noteList.childElementCount 
-
         newNote.style.background = colors[(childCount % 5)]
-
+        
         noteList.appendChild(newNote)
         newNote.appendChild(textNote)
-    }    
+        
+        empty()
+        childCount = noteList.childElementCount    
+    }
+
+    console.log(childCount);
     textarea.value = ''
     closeNote()
 }
 
 function closeNote(){
     addNoteForm.classList.add("display-none")
-    bord.classList.remove('disable')  
+    board.classList.remove('disable')  
 }
+
